abe7575cd131a82844e20473060774c974759e8d
cuiwz <cuiwzupc@163.com>
2020-03-15 09:51:08 +0800

    add heartbeat detection

--- src/main/java/com/cuiwz/netty/ChatHandler.java
+++ src/main/java/com/cuiwz/netty/ChatHandler.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
 	// 用于记录和管理所有客户端的channel
-	private static ChannelGroup users =
+	public static ChannelGroup users =
 			new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 	
 	@Override
